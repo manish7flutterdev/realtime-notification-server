feed66a98b435d6bf2e6272e9a5261482bbb7fa5
Manish Mazumdar <bboyacademician@gmail.com>
2023-01-09 23:14:23 +0530

    Second Commit

--- config.js
+++
@@ -1,5 +0,0 @@
-const  mongoDBurl= "mongodb+srv://shoppinn:TaVgl1bklg0KX14p@shoppinn.97ic6dd.mongodb.net/?retryWrites=true&w=majority"
-
-module.exports={
-    key:"breakingforchangeSecretKey",mongoDBurl
-}
